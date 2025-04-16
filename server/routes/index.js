4d1f5b39c7428703e467b50ccae1c95d87ae67ed
Alisha Taylor <inspirebyexample0@gmail.com>
2025-04-16 20:56:33 +0100

    Complete rebuild to netlify server

--- server/routes/index.js
+++
@@ -1,8 +0,0 @@
-// Replace the existing routes section with:
-const authRouter = require("./routes/auth");
-const membersRouter = require("./routes/members");
-const siegeRouter = require("./routes/siege");
-
-app.use("/api/auth", authRouter);
-app.use("/api/members", membersRouter);
-app.use("/api/siege", siegeRouter);
